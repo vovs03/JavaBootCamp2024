a6bfa5b753ca8ab3f378332e8209de7b849be66a
Vladimir Vic. Pavlychev <vladimir.keyjoo@gmail.com>
2024-06-25 09:20:54 +0300

    Initial 🐳 2024-06-25 09:20 | Day2 Summer Java_BootCamp2024

---
+++ src/main/java/ru/osarts/java/basics/README.md
@@ -0,0 +1,5 @@
+# Java Basics
+
+## Operations on primitive types
+
+- [x] `'\n' - new line, '\\' - backslash, '\r' - carriage return, '\t' - tab` [daily](https://hyperskill.org/learn/daily/1972)
---
+++ src/main/java/ru/osarts/java/basics/TemperatureConverter.java
@@ -0,0 +1,9 @@
+package ru.osarts.java.basics;
+
+public class TemperatureConverter {
+    public static void main(String[] args) {
+        double farenheit = 98.6;
+        double celsius = (farenheit) * 5 / 9;
+        System.out.println("Body temperature in Celsius: " + celsius + "\n");
+    }
+}
