acf12e70458e5014c41a00b9fbcd5f40f3921889
Vladimir Vic. Pavlychev <vladimir.keyjoo@gmail.com>
2024-06-25 10:28:15 +0300

    Fix: 🦅 task 0311 - 1,5 pomodo

--- src/main/java/ru/osarts/java/jr_tasks/HighAccuracy.java
+++ src/main/java/ru/osarts/java/jr_tasks/HighAccuracy.java
@@ -9,7 +9,7 @@ public class HighAccuracy {
         double num1 = in.nextDouble();
         double num2 = in.nextDouble();
 
-        if (num1 == num2) {
+        if (Math.abs(num1 - num2) <= 0.000_001) {
             System.out.println("Числа равны");
         } else
             System.out.println("Числа не равны");
