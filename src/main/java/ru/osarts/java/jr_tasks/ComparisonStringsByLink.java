df31517d1f45f55007117822171ffb29cdb541a0
Vladimir Vic. Pavlychev <vladimir.keyjoo@gmail.com>
2024-06-25 17:14:10 +0300

    Add: #1 🦅 Solution t 0313 ComparisonStringByLink

--- src/main/java/ru/osarts/java/jr_tasks/ComparisonStringsByLink.java
+++ src/main/java/ru/osarts/java/jr_tasks/ComparisonStringsByLink.java
@@ -1,8 +1,6 @@
 package ru.osarts.java.jr_tasks;
 // task 0313 2024-06-25 16:24
 
-import java.util.Objects;
-
 public class ComparisonStringsByLink {
     public static String string1 = "Амиго";
     public static String string2 = string1;
@@ -11,8 +9,13 @@ public class ComparisonStringsByLink {
     public static void main(String[] args) {
         String same = "ссылки на строки одинаковые";
         String different = "ссылки на строки разные";
-//        string2 = "Амуго"; // для проверки
-        if (Objects.equals(string1, string2) && Objects.equals(string2, string3) && Objects.equals(string3, string1))
+        if (string1 == string2)
+            System.out.println(same);
+        else System.out.println(different);
+        if (string2 == string3)
+            System.out.println(same);
+        else System.out.println(different);
+        if (string3 == string1)
             System.out.println(same);
         else System.out.println(different);
     }
